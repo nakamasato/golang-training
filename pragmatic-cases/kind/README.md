090f0b3683ddb1a3522ad1d7943e42d0189526ea
Masato Naka <masatonaka1989@gmail.com>
2021-11-15 06:41:26 +0900

    enable to start kind and finish at the end

--- pragmatic-cases/kind/README.md
+++ pragmatic-cases/kind/README.md
@@ -1,3 +1,5 @@
 # Set up and tear down kind
 
-
+1. Delete kind cluster. ``
+1. Create kind cluster.
+1. Defer `deleteKindCluster`
--- pragmatic-cases/kind/main.go
+++ pragmatic-cases/kind/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"fmt"
+	"os"
 	"os/exec"
-	"strings"
-	"time"
 )
 
 func main() {
@@ -14,11 +12,30 @@ func main() {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 	kindName := "test"
+	defer deleteKindCluster(ctx, kindName)
 	err := deleteKindCluster(ctx, kindName)
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
+	err = createKindCluster(ctx, kindName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// select {
+	// case <-ctx.Done():
+	// 	fmt.Println("ctx canceled")
+	// 	err := deleteKindCluster(ctx, kindName)
+	// 	if err != nil {
+	// 		fmt.Println("failed to delete cluster")
+	// 	}
+	// }
+}
+
+func createKindCluster(ctx context.Context, kindName string) error {
 	cmd := exec.CommandContext(
 		ctx,
 		"kind",
@@ -29,50 +46,11 @@ func main() {
 		"--kubeconfig",
 		"./.kubeconfig",
 	)
-	data := make(chan struct{})
-	go func() {
-		fmt.Println("start creating kind cluster")
-		if err := cmd.Run(); err != nil {
-			fmt.Println("failed to create kind cluster")
-		}
-		fmt.Println("successfully created kind cluster")
-		data <- struct{}{}
-	}()
-
-	ticker := time.NewTicker(5 * time.Second)
-	out:
-	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println("kind create cluster failed")
-			err = deleteKindCluster(ctx, kindName)
-			if err != nil {
-				return
-			}
-			break out
-		case <-data:
-			fmt.Println("kind create cluster succeeded")
-			break out
-		case t := <-ticker.C:
-			cmd := exec.CommandContext(
-				ctx,
-				"kind",
-				"get",
-				"clusters",
-			)
-			var out bytes.Buffer
-			cmd.Stdout = &out
-			err := cmd.Run()
-			if err != nil {
-				fmt.Println("faied to get kind clusters")
-			}
-			if strings.Contains(out.String(), kindName) {
-				fmt.Printf("kind cluster '%s' exists %s\n", kindName, t)
-			} else {
-				fmt.Printf("kind cluster '%s' not exists %s\n", kindName, t)
-			}
-		}
-	}
+	// cmd.Path = "."
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	fmt.Println("start creating kind cluster")
+	return cmd.Run()
 }
 
 func deleteKindCluster(ctx context.Context, kindName string) error {
@@ -84,11 +62,7 @@ func deleteKindCluster(ctx context.Context, kindName string) error {
 		"--name",
 		kindName,
 	)
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println("failed to delete kind cluster")
-		return err
-	}
-	fmt.Println("successfully deleted kind cluster")
-	return nil
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
