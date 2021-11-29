c1bacf779d4a83baf89f7e185a8d139611a25964
Masato Naka <masatonaka1989@gmail.com>
2021-11-30 06:11:58 +0900

    mysql-access

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module tmp
 go 1.16
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/prometheus/client_golang v1.11.0
 	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/oauth2 v0.0.0-20210402161424-2e8d93401602 // indirect
 	golang.org/x/sys v0.0.0-20211103184734-ae416a5f93c7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	k8s.io/apimachinery v0.22.3
 	k8s.io/client-go v0.22.3
 )
---
+++ pragmatic-cases/mysql/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 30 * time.Second)
+	defer cancel()
+	db, err := Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	row := db.QueryRow("select User from mysql.user")
+	if row.Err() != nil {
+		log.Fatal(row.Err())
+	}
+
+	res, err := checkMySQLHasUser(db, "root")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("root user exists: %t\n", res)
+}
+
+func Connect(ctx context.Context) (*sql.DB, error) {
+	data := make(chan *sql.DB, 1)
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				time.Sleep(10 * time.Second)
+				db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/")
+				if err != nil {
+					continue
+				}
+				err = db.Ping()
+				if err != nil {
+					db.Close()
+					continue
+				}
+				data <- db
+				return
+			}
+		}
+	}()
+    select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case db := <-data:
+		return db, nil
+	}
+}
+
+func checkMySQLHasUser(db *sql.DB, mysqluser string) (bool, error) {
+	row := db.QueryRow("SELECT COUNT(*) FROM mysql.user where User = '" + mysqluser + "'")
+	var count int
+	if err := row.Scan(&count); err != nil {
+	    return false, err
+	} else {
+		return count > 0, nil
+    }
+}
