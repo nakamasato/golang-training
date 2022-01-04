30322bb3b07ac3b6b866af0c2ea7aa5c794badf9
Masato Naka <masatonaka1989@gmail.com>
2022-01-05 07:27:54 +0900

    [learn go with tests][02-build-an-application] Command line & package structure Step 20: Commonize Openfile logic

--- learn-go-with-tests/02-build-an-application/cmd/webserver/main.go
+++ learn-go-with-tests/02-build-an-application/cmd/webserver/main.go
@@ -3,22 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 	"tmp/learn-go-with-tests/02-build-an-application"
 )
 
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
-	}
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
-	store, err := poker.NewFileSystemPlayerStore(db)
 	if err != nil {
-		log.Fatalf("problem creating file system player store, %v ", err)
+		log.Fatal(err)
 	}
+	defer close()
+
 	server := poker.NewPlayerServer(store)
 	// server := NewPlayerServer(NewInMemoryPlayerStore())
 	log.Fatal(http.ListenAndServe(":5000", server))
--- learn-go-with-tests/02-build-an-application/file_system_store.go
+++ learn-go-with-tests/02-build-an-application/file_system_store.go
@@ -34,6 +34,26 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 	}, nil
 }
 
+func FileSystemPlayerStoreFromFile(path string) (*FileSystemPlayerStore, func(), error) {
+	db, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("problem opening %s %v", path, err)
+	}
+
+	closeFunc := func() {
+		db.Close()
+	}
+
+	store, err := NewFileSystemPlayerStore(db)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("problem creating file system player store, %v ", err)
+	}
+
+	return store, closeFunc, nil
+}
+
 func initialisePlayerDBFile(file *os.File) error {
 	info, err := file.Stat()
 
