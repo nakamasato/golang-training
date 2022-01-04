bf07cff1b0e720a8fa7f04300216689f3ff193ec
Masato Naka <masatonaka1989@gmail.com>
2022-01-04 21:23:16 +0900

    fmt

--- learn-go-with-tests/02-build-an-application/CLI.go
+++ learn-go-with-tests/02-build-an-application/CLI.go
@@ -3,10 +3,10 @@ package poker
 import "io"
 
 type CLI struct {
-    playerStore PlayerStore
-    in io.Reader
+	playerStore PlayerStore
+	in          io.Reader
 }
 
 func (cli *CLI) PlayPoker() {
-    cli.playerStore.RecordWin("Chris")
+	cli.playerStore.RecordWin("Chris")
 }
--- learn-go-with-tests/02-build-an-application/CLI_test.go
+++ learn-go-with-tests/02-build-an-application/CLI_test.go
@@ -6,20 +6,20 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-    in := strings.NewReader("Chris wins\n")
-    playerStore := &StubPlayerStore{}
+	in := strings.NewReader("Chris wins\n")
+	playerStore := &StubPlayerStore{}
 
-    cli := &CLI{playerStore, in}
-    cli.PlayPoker()
+	cli := &CLI{playerStore, in}
+	cli.PlayPoker()
 
-    if len(playerStore.winCalls) != 1 {
-        t.Fatal("expected a win call but didn't get any")
-    }
+	if len(playerStore.winCalls) != 1 {
+		t.Fatal("expected a win call but didn't get any")
+	}
 
-    got := playerStore.winCalls[0]
-    want := "Chris"
+	got := playerStore.winCalls[0]
+	want := "Chris"
 
-    if got != want {
-        t.Errorf("didn't record correct winner, got %q, want %q", got, want)
-    }
+	if got != want {
+		t.Errorf("didn't record correct winner, got %q, want %q", got, want)
+	}
 }
