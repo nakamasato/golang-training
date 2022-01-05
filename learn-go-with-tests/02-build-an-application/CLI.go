e861ce9d371f816b556204e121430732e2335a80
Masato Naka <masatonaka1989@gmail.com>
2022-01-06 08:51:58 +0900

    [learn go with tests][02-build-an-application] Time Step 21: Periodic operation with time.AfterFunc

--- learn-go-with-tests/02-build-an-application/CLI.go
+++ learn-go-with-tests/02-build-an-application/CLI.go
@@ -2,25 +2,55 @@ package poker
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"os"
 	"strings"
+	"time"
 )
 
 type CLI struct {
 	playerStore PlayerStore
 	in          *bufio.Scanner
+	alerter     BlindAlerter
 }
 
-func NewCLI(store PlayerStore, in io.Reader) *CLI {
+type BlindAlerter interface {
+	ScheduleAlertAt(duration time.Duration, amount int)
+}
+type BlindAlerterFunc func(duration time.Duration, amount int)
+
+func (a BlindAlerterFunc) ScheduleAlertAt(duration time.Duration, amount int) {
+	a(duration, amount)
+}
+
+func StdOutAlerter(duration time.Duration, amount int) {
+	time.AfterFunc(duration, func() {
+		fmt.Fprintf(os.Stdout, "Blind is now %d\n", amount)
+	})
+}
+
+func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
 	return &CLI{
 		playerStore: store,
 		in:          bufio.NewScanner(in),
+		alerter:     alerter,
 	}
 }
 
 func (cli *CLI) PlayPoker() {
+	cli.scheduleBlindAlerts()
 	userInput := cli.readLine()
 	cli.playerStore.RecordWin(extractWinner(userInput))
+}
+
+func (cli *CLI) scheduleBlindAlerts() {
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 10 * time.Second
+	for _, blind := range blinds {
+		cli.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime = blindTime + 10*time.Second
+	}
 }
 
 func extractWinner(userInput string) string {
