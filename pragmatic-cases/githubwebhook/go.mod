7ceb950f9c52a72049120c26b7bac0f1037dfd38
Masato Naka <masatonaka1989@gmail.com>
2023-11-18 13:20:56 +0900

    feat: add GitHub Webhook handler on Cloud Run

---
+++ pragmatic-cases/githubwebhook/go.mod
@@ -0,0 +1,5 @@
+module github.com/nakamasato/golang-training/pragmatic-cases/githubwebhook
+
+go 1.20
+
+require github.com/go-playground/webhooks/v6 v6.3.0
---
+++ pragmatic-cases/githubwebhook/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"net/http"
+
+	"github.com/go-playground/webhooks/v6/github"
+)
+
+const (
+	path = "/webhooks"
+)
+
+func main() {
+	hook, _ := github.New(github.Options.Secret(os.Getenv("GITHUB_WEBHOOK_SECRET")))
+
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		payload, err := hook.Parse(r, github.ReleaseEvent, github.PullRequestEvent)
+		if err != nil {
+			if err == github.ErrEventNotFound {
+				fmt.Printf("ErrEventNotFound: %v", err)
+			}
+		}
+		switch payload := payload.(type) {
+
+		case github.CheckRunPayload:
+			fmt.Printf("CheckRunPayload: %+v", payload)
+
+		case github.StatusPayload:
+			fmt.Printf("StatusPayload: %+v", payload)
+
+		case github.PullRequestPayload:
+			fmt.Printf("PullRequestPayload: %+v", payload)
+		}
+	})
+	http.ListenAndServe(":8080", nil)
+}
